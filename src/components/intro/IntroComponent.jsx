2b846a0f83db744ae241cd6aaae340635a002d30
Hemanth Mudra <47498538+hmswrth@users.noreply.github.com>
2022-11-07 11:10:47 +0530

    added css rubberband on intro

--- src/components/intro/IntroComponent.jsx
+++ src/components/intro/IntroComponent.jsx
@@ -1,31 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Intro.css";
+import "animate.css";
+import hLogo from "../../assets/hLogo.svg";
 
 function IntroComponent() {
   let intro = "Hi, I'm Hemanth, Full Stack Web Dev";
+  const [isHovering, setIsHovering] = useState(false);
+  const [hoveringIndex, setHoveringIndex] = useState(null);
+
+  // handler to check mouse hover
+  const handleMouseOver = (hoveringIndex) => {
+    setIsHovering(true);
+    setHoveringIndex(hoveringIndex);
+  };
+
+  // handler to check mouse hover
+  const handleMouseOut = () => {
+    setIsHovering(false);
+    setHoveringIndex(null);
+  };
+
   return (
     <>
-      <span className="tag">{"<body>"}</span>
-      <h1>
+      <p className="tag animate__animated animate__bounce animate__repeat-1 margin-none">
+        {"<body>"}
+      </p>
+      <div style={{ height: "100vh" }}>
         {[...Array(intro.length)].map((_, index) => {
-          {
-            /* intro.charAt(index) !== ',' ? */
-          }
           return intro.charAt(index) !== "," ? (
-            <span className="color-white font-coolvetica intro-text" key={index}
-            >
-              {intro.charAt(index)}
-            </span>
+            index > 2 && intro.charAt(index) === "H" ? (
+              <img className="hlogo" src={hLogo} alt="" />
+            ) : (
+              <span
+                onMouseOver={() => handleMouseOver(index)}
+                onMouseOut={() => handleMouseOut(index)}
+                className={`color-white font-coolvetica intro-text ${
+                  intro.charAt(index) === " " && "txt-dec-none"
+                } ${
+                  isHovering &&
+                  hoveringIndex === index &&
+                  "animate__animated animate__rubberBand animate__repeat-1"
+                }`}
+                key={index}
+              >
+                {intro.charAt(index)}
+              </span>
+            )
+          ) : intro.charAt(index) === " " ? (
+            <span className="txt-dec-none">{intro.charAt(index)}</span>
           ) : (
             <>
-              <span className="color-white font-coolvetica intro-text" key={index}>
+              <span
+                className={`animate__animated animate__rubberBand color-white font-coolvetica intro-text ${
+                  isHovering &&
+                  hoveringIndex === index &&
+                  "animate__animated animate__bounce animate__repeat-1"
+                }`}
+                key={index}
+              >
                 {intro.charAt(index)}
               </span>
               <br />
             </>
           );
         })}
-      </h1>
+      </div>
     </>
   );
 }
